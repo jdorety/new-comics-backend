90ff877d866478b42090382cbe5fb72b5fc40010
Jonathan Dorety <kyullog@gmail.com>
2020-01-20 15:30:31 -0800

    Create endpoint for previous week API resource

--- routers/newComics.js
+++ routers/newComics.js
@@ -3,8 +3,10 @@ const axios = require("axios");
 
 const router = express.Router();
 
+const API = process.env.API; // Shortbox API root URL
+
+// gets new comics for current week
 router.get("/", async (req, res) => {
-  const API = process.env.API; // Shortbox API root URL
   try {
     // make GET request to Shortbox API current release week resource
     const newComics = await axios.get(`${API}/comics/v1/new`);
@@ -17,5 +19,18 @@ router.get("/", async (req, res) => {
     res.status(500).send(err);
   }
 });
+
+// gets new comics for previous week
+router.get("/previous", async (req, res) => {
+  try {
+    const newComics = await axios.get(`${API}/comics/v1/previous`);
+    res.status(200).send({ comics: newComics.data.comics });
+  } catch (err) {
+    console.log(err);
+    res.status(500).send(err);
+  }
+});
+
+
 
 module.exports = router;
