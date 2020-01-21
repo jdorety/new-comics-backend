2d42d6ab46bc4f8c0fe6420f221ba0f875db3d83
Jonathan Dorety <kyullog@gmail.com>
2020-01-20 21:22:51 -0800

    Create experimental cover scraper functions

--- routers/newComics.js
+++ routers/newComics.js
@@ -1,16 +1,47 @@
 const express = require("express");
 const axios = require("axios");
+const cheerio = require("cheerio");
 
 const router = express.Router();
 
 const API = process.env.API; // Shortbox API root URL
+
+
+const getCoverUrl = async diamondId => {
+  try {
+    const result = await axios.get(
+      `https://previewsworld.com/Catalog/${diamondId}`
+    );
+    return cheerio.load(result.data);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const addCover = async (url, diamondId) => {
+  try {
+    const previewsPage = await getCoverUrl(diamondId);
+    const imageEndpoint = previewsPage("#MainContentImage").attr().src
+    const imageUrl = `${url}${imageEndpoint}`
+    console.log(imageUrl);
+    return imageUrl;
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+
 
 // gets new comics for current week
 router.get("/", async (req, res) => {
   try {
     // make GET request to Shortbox API current release week resource
     const newComics = await axios.get(`${API}/comics/v1/new`);
-    console.log(newComics);
+    const { comics } = newComics.data;
+    for (comic of comics) {
+      const imageSrc = await addCover("https://previewsworld.com",comic.diamond_id)
+      comic.cover_url = imageSrc
+    }
     // responds with array of all current week releases
     res.status(200).send({ comics: newComics.data.comics });
   } catch (err) {
