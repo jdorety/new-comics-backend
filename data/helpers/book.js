5a1e10a11b7c5e9a6a0cf30396e7a450baca3913
Jonathan Dorety <kyullog@gmail.com>
2020-01-27 09:51:58 -0800

    create addBook helper

--- data/helpers/book.js
+++ data/helpers/book.js
@@ -1,7 +1,19 @@
 const db = require("../dbConfig");
 
-module.exports(getAll);
+module.exports(getAll, addBook);
 
 function getAll() {
   return db.select().table("book");
+}
+
+function addBook(book) {
+  return db("book").insert({
+    title: book.title,
+    publisher_id: book.publisher_id,
+    description: book.description,
+    creators: book.creators,
+    price: book.price,
+    release_date: book.release_date,
+    diamond_id: book.diamond_id
+  });
 }
