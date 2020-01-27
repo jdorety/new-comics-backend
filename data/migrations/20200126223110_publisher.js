7adcaeed2cdf43ebbf6bd287674cdfe5de8c1bda
Jonathan Dorety <kyullog@gmail.com>
2020-01-26 22:48:38 -0800

    create "publisher" table schema

--- data/migrations/20200126223110_publisher.js
+++ data/migrations/20200126223110_publisher.js
@@ -1,8 +1,13 @@
-
 exports.up = function(knex) {
-  
+  return knex.schema.createTable("publisher", tbl => {
+    tbl.increments();
+    tbl
+      .string("pub_name")
+      .unique()
+      .notNullable();
+  });
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTableIfExists("publisher");
 };
