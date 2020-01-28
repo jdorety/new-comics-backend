6565d78c3ad7e023328411893b027d1588fe1a0e
Jonathan Dorety <kyullog@gmail.com>
2020-01-27 21:24:51 -0800

    DB testing initial config

--- knexfile.js
+++ knexfile.js
@@ -46,5 +46,18 @@ module.exports = {
       directory: "./data/seeds"
     },
     useNullAsDefault: true
+  },
+  testing: {
+    client: "sqlite3",
+    connection: {
+      filename: "./data/test.sqlite3"
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: "./data/migrations"
+    },
+    seeds: {
+      directory: "./data/seeds"
+    }
   }
 };
