0a67497afdb42799ce6191adadc0126ba5dd50df
Jonathan Dorety <kyullog@gmail.com>
2020-01-27 19:00:22 -0800

    refactor for deployment

--- data/dbConfig.js
+++ data/dbConfig.js
@@ -2,5 +2,6 @@ const knex = require("knex");
 const knexConfig = require("../knexfile.js");
 
 const environment = process.env.ENVIRONMENT || "development";
+const configOptions = knexConfig[environment];
 
-module.exports = knex(knexConfig[environment]);
+module.exports = knex(configOptions);
